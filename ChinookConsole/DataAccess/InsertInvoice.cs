029147d3d13ca7f8da20bda05405a6ef5641a71b
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-02-26 19:25:53 -0600

    can update

---
+++ ChinookConsole/DataAccess/InsertInvoice.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookConsole.DataAccess
+{
+    class InsertInvoice
+    {
+        readonly string _connectionString = ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString;
+
+        public bool Insert(int customerId, string billingAddress, string billingCity, string billingState, string billingPostalCode, double total)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Invoice (
+          
+           CustomerId,
+           InvoiceDate,
+           BillingAddress,
+           BillingCity,
+           BillingState,
+           BillingCountry,
+           BillingPostalCode,
+           Total)
+     VALUES
+           (
+           @CustomerId,
+           3/13/1981, 
+           @BillingAddress, 
+           @BillingCity, 
+           @BillingState, 
+           'USA', 
+           @BillingPostalCode, 
+           @Total)";
+
+               
+
+                var customerIdParam = new SqlParameter("@CustomerId", SqlDbType.Int);
+                customerIdParam.Value = customerId;
+                cmd.Parameters.Add(customerIdParam);
+
+                var billingAddressParam = new SqlParameter("@BillingAddress", SqlDbType.NVarChar);
+                billingAddressParam.Value = billingAddress;
+                cmd.Parameters.Add(billingAddressParam);
+
+                var billingCityParam = new SqlParameter("@BillingCity", SqlDbType.NVarChar);
+                billingCityParam.Value = billingCity;
+                cmd.Parameters.Add(billingCityParam);
+
+                var billingStateParam = new SqlParameter("@BillingState", SqlDbType.NVarChar);
+                billingStateParam.Value = billingState;
+                cmd.Parameters.Add(billingStateParam);
+
+                var billingPostalCodeParam = new SqlParameter("@BillingPostalCode", SqlDbType.NVarChar);
+                billingPostalCodeParam.Value = billingPostalCode;
+                cmd.Parameters.Add(billingPostalCodeParam);
+
+                var totalParam = new SqlParameter("@Total", SqlDbType.Decimal);
+                totalParam.Value = total;
+                cmd.Parameters.Add(totalParam);
+
+                connection.Open();
+
+                var result = cmd.ExecuteNonQuery();
+
+                return result == 1;
+            }
+        }
+    }
+}
--- ChinookConsole/Program.cs
+++ ChinookConsole/Program.cs
@@ -11,24 +11,48 @@ namespace ChinookConsole
     {
         static void Main(string[] args)
         {
-            var invoiceQuery = new InvoiceQuery();
-            var invoices = invoiceQuery.GetInvoiceDetails();
+            //var invoiceQuery = new InvoiceQuery();
+            //var invoices = invoiceQuery.GetInvoiceDetails();
 
-            foreach (var invoice in invoices)
-            {
-                Console.WriteLine($"The invoice totaling {invoice.Total} was shipped to {invoice.Company} in {invoice.Country}.  Their Sales agent is {invoice.FirstName} {invoice.LastName}.");
-            }
-            Console.ReadLine();
+            //foreach (var invoice in invoices)
+            //{
+            //    Console.WriteLine($"The invoice totaling {invoice.Total} was shipped to {invoice.Company} in {invoice.Country}.  Their Sales agent is {invoice.FirstName} {invoice.LastName}.");
+            //}
+            //Console.ReadLine();
 
-            var invoiceToSalesAgentQuery = new InvoiceToSalesAgentQuery();
-            var invoices2 = invoiceToSalesAgentQuery.GetInvoiceBySalesAgent();
+            //var invoiceToSalesAgentQuery = new InvoiceToSalesAgentQuery();
+            //var invoices2 = invoiceToSalesAgentQuery.GetInvoiceBySalesAgent();
 
-            foreach (var invoice in invoices2)
-            {
-                Console.WriteLine($"{invoice.FirstName} {invoice.LastName}'s invoice Id is {invoice.InvoiceId} ");
-            }
-            Console.ReadLine();
+            //foreach (var invoice in invoices2)
+            //{
+            //    Console.WriteLine($"{invoice.FirstName} {invoice.LastName}'s invoice Id is {invoice.InvoiceId} ");
+            //}
+            //Console.ReadLine();
 
+           
+
+            Console.WriteLine("What is your customer Id?");
+            var customerId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your Billing Address?");
+            var billingAddress = Console.ReadLine();
+
+            Console.WriteLine("What is your billing city?");
+            var billingCity = Console.ReadLine();
+
+            Console.WriteLine("What is your billing state?");
+            var billingState = Console.ReadLine();
+
+            Console.WriteLine("What is your Zip");
+            var postalCode = Console.ReadLine();
+
+            Console.WriteLine("What is the invoice total?");
+            var total = double.Parse(Console.ReadLine());
+
+            var newInvoice = new InsertInvoice();
+            newInvoice.Insert(customerId, billingAddress, billingCity, billingState, postalCode, total);
+
+            
 
 
         }
