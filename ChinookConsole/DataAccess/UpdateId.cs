59872528b283e85255ca79eebf3bcf7f5d6d5e76
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-03-23 20:38:50 -0500

    updating

--- ChinookConsole/DataAccess/UpdateId.cs
+++ ChinookConsole/DataAccess/UpdateId.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,32 @@ namespace ChinookConsole.DataAccess
 {
     class UpdateId
     {
+        readonly string _connectionString = ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString;
+
+        public bool UpdateId(int employeeId, string FirstName, string LastName)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"
+
+        UPDATE employee
+        SET 
+        LastName = nvarchar,
+        FirstName, nvarchar
+      
+ WHERE employeeId = @employeeId";
     }
-}
+
+            var employeeIdParam = new SqlParameter("@EmployeeId", SqlDbType.Int);
+            employeeIdParam.Value = employeeId;
+            cmd.Parameters.Add(employeeIdParam);
+
+            var firstNameParam = new SqlParameter("@FirstName", SqlDbType.Int);
+            firstNameParam.Value = firstName;
+            cmd.Parameters.Add(firstNameParam);
+
+            var lastNameParam = new SqlParameter("@LastName", SqlDbType.Int);
+            lastNameParam.Value = lastName;
+            cmd.Parameters.Add(lastNameParam);
+        }
